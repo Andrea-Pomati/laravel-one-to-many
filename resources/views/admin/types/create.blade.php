54894dc1c6dac8d4a5723b7ffce6c7e37c43698b
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-24 17:38:35 +0200

    feat: added name and description error

--- resources/views/admin/types/create.blade.php
+++ resources/views/admin/types/create.blade.php
@@ -11,12 +11,22 @@
         
         <div class="mb-3">
             <label for="name">Nome</label>
-            <input id="name" name="name" class="form-control" type="text">
+            <input id="name" name="name" class="form-control @error('name') is-invalid @enderror" type="text" value="{{old('name')}}">
+            @error('name')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
         
         <div class="mb-3">
             <label for="description">Descrizione</label>
-            <textarea id="description" name="description" class="form-control"></textarea>
+            <textarea id="description" name="description" class="form-control @error('description') is-invalid @enderror">{{old('description')}}</textarea>
+            @error('description')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
         
         <button class="btn btn-primary" type="submit">Aggiungi</button>
