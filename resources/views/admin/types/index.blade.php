b66f9e17691bbfb3e3ff5056aeb045e60fceccf7
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-24 14:38:35 +0200

    feat: added table

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -4,6 +4,29 @@
 
     <div class>
         <h1>Tipi</h1>
+
+        <table class="table table-striped">
+            <thead>
+                <th>Nome</th>
+                <th>Descrizione</th>
+                <th>Slug</th>
+                <th></th>
+            </thead>
+
+            <tbody>
+
+                @foreach($types as $type)
+                <tr>
+                    <td>{{$type->name}}</td>
+                    <td>{{$type->description}}</td>
+                    <td>{{$type->slug}}</td>
+                    <td><a href="{{route('admin.types.show', $type)}}"><i class="fa-solid fa-magnifying-glass"></i></a></td>
+                </tr>
+                @endforeach
+            </tbody>
+
+
+        </table>
     </div>
 
 @endsection
