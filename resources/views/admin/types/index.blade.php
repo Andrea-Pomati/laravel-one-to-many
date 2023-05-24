914ad16345e3d89a082004d6bc6b86349015212d
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-24 15:31:11 +0200

    feat: added th and td

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -10,6 +10,7 @@
                 <th>Nome</th>
                 <th>Descrizione</th>
                 <th>Slug</th>
+                <th>N° progetti</th>
                 <th>Comandi</th>
             </thead>
 
@@ -20,6 +21,7 @@
                     <td>{{$type->name}}</td>
                     <td>{{$type->description}}</td>
                     <td>{{$type->slug}}</td>
+                    <td>{{ count($type->projects) }}</td>
                     <td><a href="{{route('admin.types.show', $type)}}"><i class="fa-solid fa-magnifying-glass"></i></a></td>
                 </tr>
                 @endforeach
